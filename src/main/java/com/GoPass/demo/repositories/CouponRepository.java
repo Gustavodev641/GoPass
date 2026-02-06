54d1d7f134f81e534213150d0be78c10f670ebb1
Gustavo Nascimento <gustavopdn1@gmail.com>
2026-02-06 12:57:46 -0300

    feat: add repositories

---
+++ src/main/java/com/GoPass/demo/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.GoPass.demo.repositories;
+
+import com.GoPass.demo.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
